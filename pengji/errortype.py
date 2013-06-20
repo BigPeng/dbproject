d73234717b3c78a875b9ebc3d335174dd1c8a6eb
pengji <jiqunpeng@gmail.com>
2013-06-20 19:55:33 +0800

    remove all

--- pengji/errortype.py
+++
@@ -1,9 +0,0 @@
-
-
-class SqlError(Exception):
-    def __init__(self,value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
-NO_SUCH_TABLE = 0x800
-NO_SUCH_ATTR = 0x801
